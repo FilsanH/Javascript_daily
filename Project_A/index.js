fb87230885f7135feddbb992f5d060d1e972b3bb
Filsan Hassan <fh363.cam.ac.uk>
2020-08-08 20:59:39 +0100

    click me changes so background is colour pallet

--- Project_A/index.js
+++ Project_A/index.js
@@ -3,7 +3,7 @@
 //place CDN at the bottom of html
 
 // flick through pallete
-const colours = [
+let colours = [
   '#C2185B',
   '#F8BBD0',
   '#FFFFFF',
@@ -20,12 +20,9 @@ const imgs = ['sky1', 'sky2', 'sky3', 'sky7', 'sky8']
 let count = 0
 
 document.querySelector('.button').addEventListener('click', () => {
-  document.body.style.backgroundColor = colours[count]
+  console.log(colours[count])
+  document.body.style.backgroundColor = `rgb(${colours[count]})`
   count === colours.length ? (count = 0) : count++
-  document.querySelector('img')
-  if (imgs[count]) {
-    document.querySelector('img').src = `img/${imgs[count]}.jpg`
-  }
 })
 
 const colorThief = new ColorThief()
@@ -36,8 +33,13 @@ document.querySelector('img').addEventListener('click', (e) => {
   if (img.complete) {
     const color = colorThief.getColor(img)
     console.log(`here ${color}`)
-    console.log(colorThief.getPalette(img,6))   ///now loop through array when click me is pressed 
+    colours = colorThief.getPalette(img, 6) ///now loop through array when click me is pressed
+    console.log(colours)
     document.body.style.backgroundColor = `rgb(${color})`
+    document.querySelector('img')
+    if (imgs[count]) {
+      document.querySelector('img').src = `img/${imgs[count]}.jpg`
+    }
   } else {
     img.addEventListener('load', function () {
       console.log(colorThief.getColor(img))

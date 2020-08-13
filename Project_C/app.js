87567131aa839353dc1412e9f06b071cb380c87a
Filsan Hassan <fh363.cam.ac.uk>
2020-08-13 18:15:12 +0100

    navbar

--- Project_C/app.js
+++ Project_C/app.js
@@ -1,1 +1,23 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+let count = 0
+// find all the buttons and adujust the count accordingly
+// seeing as all of the event handlers are doing pretty much the same thing it is best to do query selector all and then filter through them accordingly
+
+const btns = document.querySelectorAll('.btn')
+const counter = document.querySelector('#value')
+
+console.log(btns[0])
+//want to filter out all the nodelist returned by class do this by using forEach method with .classlist
+
+btns.forEach(function (cur, index, listObj) {
+  console.log(cur + ', ' + index + ', ' + this)
+  cur.addEventListener('click', () => {
+    if (cur.classList.contains('decrease')) {
+      count--
+    } else if (cur.classList.contains('increase')) {
+      count++
+    } else {
+      count = 0
+    }
+    counter.innerHTML = count
+  })
+})
